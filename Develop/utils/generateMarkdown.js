e5911248c0edd8b0ad140113450bab1f91ca2109
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-04-02 15:59:32 -0400

    Initialized the ReadMe code, however having a problem with the license badge showing up

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,13 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  const badges = {
-    mit: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
-    apache: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
-    boost: `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`,
+  switch (license)  {
+    case "mit":
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      break;
+
+    case "apache":
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      break;
+
+    case "boost": 
+      return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+      break;
+
+    case "none":
+      return ""
+      break;
   }
-  return badges[license]
-}
+  
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -15,7 +27,7 @@ function renderLicenseLink(license) {
   if (license == 'N/A') {
     return ''
   } else return `[License](#license)`
-}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -23,13 +35,14 @@ function renderLicenseSection(license) {
   if (license == 'N/A'){
     return ''
   } else return `Licensed under the ${license} license`
-}
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ${renderLicenseBadge(data.license)}
+  return `# ${data.title} 
+  ${renderLicenseBadge(data.license)}
 
-  ## Description
+  ## Description 
   ${data.description}
 
   ## Table of Contents
